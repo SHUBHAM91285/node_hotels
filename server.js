a2cc505702dc2665235eec3bbcf9cd92771d051e
SHUBHAM91285 <tshubhamnath@gmail.com>
2024-02-06 23:49:26 +0530

    Shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());//it stores the data in req.body, the data which is send by postman
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('Welcome to our Hotel.')
@@ -15,6 +17,6 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server is running on port: 3000");
 })
